dddd2db95656127e23068e82c1e1de310b043711
ceciliaxli <34094679+ceciliaxli@users.noreply.github.com>
2019-04-23 14:18:26 +1000

    Update delay.js

--- delay.js
+++ delay.js
@@ -5,10 +5,28 @@ var textCount = text.length;
 document.getElementById("delay").innerHTML = "";
 var i = 0;
 //create a variable of the setInterval function so we can call it during clearInterval if the i counter hits the end of the text length.  Otherwise, this will run infinitely.
+var typeText = setTimeout(setInterval(function () {
+    document.getElementById("delay").innerHTML += text.substring(i, i + 1);
+    i++;
+    if (i >= textCount) {
+        clearInterval(typeText);
+    }
+}, 200),3000);
+
+
+/*
+//get original text from element
+var text = document.getElementById("delay").innerHTML;
+var textCount = text.length;
+//clear the element so we can type it out
+document.getElementById("delay").innerHTML = "";
+var i = 0;
+//create a variable of the setInterval function so we can call it during clearInterval if the i counter hits the end of the text length.  Otherwise, this will run infinitely.
 var typeText = setInterval(function () {
     document.getElementById("delay").innerHTML += text.substring(i, i + 1);
     i++;
     if (i >= textCount) {
         clearInterval(typeText);
     }
-}, 0);
+}, 200);
+*/
