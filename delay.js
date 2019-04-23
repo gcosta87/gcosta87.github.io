1f7cfa38d9d487b13633b836a28dec6847f3a9d6
ceciliaxli <34094679+ceciliaxli@users.noreply.github.com>
2019-04-23 13:53:08 +1000

    Update and rename typewriter_delay.js to delay.js

--- delay.js
+++ delay.js
@@ -1,12 +1,12 @@
 //get original text from element
-var text = document.getElementById("typewriter_delay").innerHTML;
+var text = document.getElementById("delay").innerHTML;
 var textCount = text.length;
 //clear the element so we can type it out
-document.getElementById("typewriter_delay").innerHTML = "";
+document.getElementById("delay").innerHTML = "";
 var i = 0;
 //create a variable of the setInterval function so we can call it during clearInterval if the i counter hits the end of the text length.  Otherwise, this will run infinitely.
 var typeText = setInterval(function () {
-    document.getElementById("typewriter_delay").innerHTML += text.substring(i, i + 1);
+    document.getElementById("delay").innerHTML += text.substring(i, i + 1);
     i++;
     if (i >= textCount) {
         clearInterval(typeText);
