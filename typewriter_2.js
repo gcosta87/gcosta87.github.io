476c9ce25979596c66fd18a1d04923d5d25606a6
ceciliaxli <34094679+ceciliaxli@users.noreply.github.com>
2019-04-23 13:37:48 +1000

    Create typewriter_2.js

---
+++ typewriter_2.js
@@ -0,0 +1,14 @@
+//get original text from element
+var text = document.getElementById("typewriter").innerHTML;
+var textCount = text.length;
+//clear the element so we can type it out
+document.getElementById("typewriter").innerHTML = "";
+var i = 0;
+//create a variable of the setInterval function so we can call it during clearInterval if the i counter hits the end of the text length.  Otherwise, this will run infinitely.
+var typeText = setInterval(function () {
+    document.getElementById("typewriter").innerHTML += text.substring(i, i + 1);
+    i++;
+    if (i >= textCount) {
+        clearInterval(typeText);
+    }
+}, 5000);
