2c1d0f7456cb44bc97818ac92714b220ceb8f878
Gonzalo Gabriel Costa <gonzalogcosta@yahoo.com.ar>
2020-03-04 01:27:29 -0300

    Commit inicial
    Añadido:
      - Estructura basica del sitio (carpetas).
      - PDF CV vacio.
    
    Cambios:
      - favicon.ico: "logo" de GitHub.
      - style.css: pequeños cambios de estilo y fuente (Google Note). Añadida nueva seccion «under construction».
      - index.html: Eliminacion temporal de datos personales previos. Adición de jQuery. Sustitucion de recursos.
      - Cambios varios.

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+    title:'',
+    /**
+     * Define if the site/porfolio is "under construction"
+     */
+    under_construction: {
+        //boolean value. true: enable, false=dishabled
+        active:     true,
+
+        fa_icon_class:  'fas fa-laptop-code',
+
+        header:         'este sitio está en construción',
+        description:    '(tenga paciencia y vuelva pronto... )'
+    },
+
+};
